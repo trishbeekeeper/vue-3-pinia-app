8f287e7683fcfb58d853a8ade7deb788505b7e5b
Trish Beekeeper <chickabuzz@gmail.com>
2022-12-17 20:04:20 -0500

    new user can be made!

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,19 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+//import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
+  
   <header>
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <!--<HelloWorld msg="I did it! You have no idea...." /> -->
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/login">Login</RouterLink>
+        <RouterLink to="/register">Register New User</RouterLink>
       </nav>
     </div>
   </header>
